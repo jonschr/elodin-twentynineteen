215a8e1b7d0581e93660d116667c9df0bc8fe893
jonschr <jonschr@gmail.com>
2019-12-11 14:36:10 -0600

    Landing page template set up

--- page_landing.php
+++ page_landing.php
@@ -12,17 +12,10 @@
  * @link    https://www.studiopress.com/
  */
 
+ //* Add the landing page class
 add_filter( 'body_class', 'genesis_sample_add_body_class' );
-/**
- * Adds landing page body class.
- *
- * @since 1.0.0
- *
- * @param array $classes Original body classes.
- * @return array Modified body classes.
- */
 function genesis_sample_add_body_class( $classes ) {
-
+	
 	$classes[] = 'landing-page';
 	return $classes;
 
@@ -31,39 +24,35 @@ function genesis_sample_add_body_class( $classes ) {
 // Removes Skip Links.
 remove_action( 'genesis_before_header', 'genesis_skip_links', 5 );
 
+//* Dequeue skip links
 add_action( 'wp_enqueue_scripts', 'genesis_sample_dequeue_skip_links' );
-/**
- * Dequeues Skip Links Script.
- *
- * @since 1.0.0
- */
 function genesis_sample_dequeue_skip_links() {
 
 	wp_dequeue_script( 'skip-links' );
 
 }
 
-// Forces full width content layout.
-add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
+//* Forces full width content layout.
+// add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
 
-// Removes site header elements.
-remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
+//* Removes site header elements.
+remove_action( 'genesis_header', 'elodin_header_markup_open', 5 );
 remove_action( 'genesis_header', 'genesis_do_header' );
 remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
 
-// Removes navigation.
+//* Removes navigation.
 remove_theme_support( 'genesis-menus' );
 
-// Removes breadcrumbs.
+//* Removes breadcrumbs.
 remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
 
-// Removes footer widgets.
+//* Removes footer widgets.
 remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
 
-// Removes site footer elements.
+//* Removes site footer elements.
 remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
 remove_action( 'genesis_footer', 'genesis_do_footer' );
 remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
 
-// Runs the Genesis loop.
+//* Runs the Genesis loop.
 genesis();
